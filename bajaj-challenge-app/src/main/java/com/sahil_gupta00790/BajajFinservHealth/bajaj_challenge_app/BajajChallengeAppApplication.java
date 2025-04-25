a8e4391ce2fe68aff89b1e9f9813f1abc655ef51
Sahil Gupta <sahil.gupta.150520@gmail.com>
2025-04-25 16:40:31 +0530

    Initial Spring Boot setup

---
+++ bajaj-challenge-app/src/main/java/com/sahil_gupta00790/BajajFinservHealth/bajaj_challenge_app/BajajChallengeAppApplication.java
@@ -0,0 +1,13 @@
+package com.sahil_gupta00790.BajajFinservHealth.bajaj_challenge_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajChallengeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajChallengeAppApplication.class, args);
+	}
+
+}
---
+++ bajaj-challenge-app/src/test/java/com/sahil_gupta00790/BajajFinservHealth/bajaj_challenge_app/BajajChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil_gupta00790.BajajFinservHealth.bajaj_challenge_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
